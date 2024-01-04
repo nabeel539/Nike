0ce70987a164d787e5a4b43a5f5d263824225225
MD Nabeel Ahemad <mdnabeelahemad539@gmail.com>
2024-01-04 11:16:49 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,23 @@
+import Nav from './Nav'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Servies from './Servies'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+
+
+export {
+    Nav,
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Servies,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
